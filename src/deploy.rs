54786bd2c0b7c93bfe247b2f401de0c45ab0f9d3
SanderLoman <sanderfeitsma13@gmail.com>
2023-02-25 00:46:39 +0100

    bruh

--- src/deploy.rs
+++ src/deploy.rs
@@ -1,3 +1,19 @@
-pub fn deploy() {
-    println!("");
+#![deny(unsafe_code)]
+
+use dotenv::dotenv;
+use ethers::prelude::*;
+use eyre::Result;
+// use url::Url;
+// use hex;
+
+pub fn deploy() -> Result<()> {
+    dotenv().ok();
+    println!("Deploying contracts.");
+
+    let deployer_priv_key: String =
+        std::env::var("TESTWALLET_PRIVATE_KEY").expect("TESTWALLET_PRIVATE_KEY must be set");
+
+    let deployer = deployer_priv_key.parse::<LocalWallet>()?;
+    println!("Deployer: {:?}", deployer.address());
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 // Work on contract deployment script.
 // Make a simple transaction happen using flashbots.
-// watch https://www.youtube.com/watch?v=wn8r674U1B4&t=3171s&ab_channel=RobertMiller aboput simple arbitrage
 
 #![deny(unsafe_code)]
 
 use dotenv::dotenv;
-use ethers::{
-    prelude::{rand::thread_rng, *},
-};
+use ethers::prelude::{rand::thread_rng, *};
 use ethers_flashbots::*;
 use eyre::Result;
 use url::Url;
+// use hex;
 // use std::convert::TryFrom;
 // use ethabi::{Contract, Token};
 // use std::fs;
-use ethers_core::utils::{Ganache, GanacheInstance};
 
 mod addresses;
 mod arbitrage;
@@ -23,13 +20,13 @@ mod deploy;
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
-    println!("Running MEV Bot");
+
     deploy::deploy();
     arbitrage::arbitrage();
     addresses::addresses();
 
     let eth_ws_url: String = std::env::var("ETH_WS_URL").expect("ETH_WS_URL must be set");
-    let url:Url = Url::parse(&eth_ws_url)?;
+    let url: Url = Url::parse(&eth_ws_url)?;
     let provider: Provider<Ws> = Provider::<Ws>::connect(url).await?;
 
     let test_wallet_private_key: String =
@@ -50,14 +47,6 @@ async fn main() -> Result<()> {
         wallet_address,
     );
     println!("Client: {:?}\n", client);
-
-    let port:u16 = 8545;
-    let fork:Url = Url::parse(&eth_ws_url)?;
-    // let mut ganache: GanacheInstance = Ganache::new().spawn();
-    let mut ganache:Ganache = Ganache::new().port(port).fork(fork);
-    let test123:String = serde_json::to_string(&ganache)?;
-    
-    // maybe work with ganache and ganacheInstance together
 
     Ok(())
 }
