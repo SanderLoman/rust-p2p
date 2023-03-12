1ab94b3b5d40def9ee6115b0ca58b87c80945e57
SanderLoman <sanderfeitsma13@gmail.com>
2023-03-12 18:09:15 +0100

    push

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,35 @@
 #![deny(unsafe_code)]
 
 use dotenv::dotenv;
-// use ethers::prelude::{rand::thread_rng, *};
-// use ethers_flashbots::*;
+use ethabi::Contract;
+use ethers::prelude::*;
 use eyre::Result;
-// use std::convert::TryFrom;
-// use ethers::utils::Ganache;
-// use url::Url;
-
-// use hex;
-// use std::convert::TryFrom;
-// use ethabi::{Contract, Token};
-// use std::fs;
+use std::fs;
 
 mod abi;
 mod addresses;
 mod arbitrage;
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() -> Result<()> {
     dotenv().ok();
+
+    let eth_rpc_url: String = std::env::var("ETH_WS_URL").expect("ETH_WS_URL must be set");
+    
+    let provider_eth = Provider::<Ws>::connect(eth_rpc_url).await?;
+    let sub  = provider_eth.watch_pending_transactions().await?;
+    
+    sub.for_each(|tx| async move {
+        println!("New pending transaction: https://etherscan.io/tx/{:?}", tx);
+    }).await;
+
+    // let ethereum_ca: Address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".parse()?;
+    // let read_ethereum_abi: String = fs::read_to_string("abis/ethereum.json")?;
+    // let abi = Contract::load(read_ethereum_abi.as_bytes())?;
+
+    // let ethereum_contract = ethers::contract::Contract::new(ethereum_ca, abi, provider_eth);
+
+
     abi::abis();
     addresses::addresses();
     arbitrage::arbitrage();
