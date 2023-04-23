28d28dce531d62d0380fbb907c332141d7dd059d
SanderLoman <sanderfeitsma13@gmail.com>
2023-04-23 23:02:16 +0200

    added time_to_reach_node function

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 // use url::Url;
 
 #![deny(unsafe_code)]
-use chrono::{DateTime, Local};
+use chrono::{DateTime, Local, TimeZone, Utc};
 use colored::*;
 use dotenv::dotenv;
 use ethers::prelude::*;
@@ -14,6 +14,7 @@ use serde_json::Value;
 
 use std::fmt;
 use std::path::Path;
+use std::sync::Arc;
 
 use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::net::UnixStream;
@@ -62,9 +63,15 @@ async fn main() -> Result<()> {
     let geth_rpc_endpoint: &str = "/home/sander/.ethereum/goerli/geth.ipc";
 
     // Later we will push to this vec when we get the enode urls from the geth nodes
-    let static_nodes: Vec<&str> = vec![];
+    let static_nodes_remove: Vec<&str> = vec![];
 
-    for enode_url in static_nodes {
+    let static_nodes_add: Vec<&str> = vec![];
+
+    for enode_url in static_nodes_remove {
+        delete_peer(Path::new(geth_rpc_endpoint), enode_url).await?;
+    }
+
+    for enode_url in static_nodes_add {
         add_peer(Path::new(geth_rpc_endpoint), enode_url).await?;
     }
 
@@ -75,6 +82,10 @@ async fn main() -> Result<()> {
         std::env::var("LOCAL_HOST_URL").expect("LOCAL_HOST_URL must be set");
 
     let provider: Provider<Ws> = Provider::<Ws>::connect(localhost_rpc_url).await?;
+    let provider_arc = Arc::new(provider.clone());
+
+    time_to_reach_node(provider_arc).await?;
+
     // let block_number: U64 = provider.get_block_number().await?;
     let gas_price: U256 = provider.get_gas_price().await?;
 
@@ -227,3 +238,40 @@ async fn delete_peer(ipc_path: &Path, enode_url: &str) -> Result<()> {
 }
 
 async fn find_node() {}
+
+async fn time_to_reach_node(provider: Arc<Provider<Ws>>) -> Result<()> {
+    let mut stream: SubscriptionStream<Ws, Block<TxHash>> = provider.subscribe_blocks().await?;
+
+    while let Some(block_header) = stream.next().await {
+        let block_timestamp: U256 = block_header.timestamp;
+        let block_time: chrono::LocalResult<DateTime<Utc>> =
+            Utc.timestamp_opt(block_timestamp.as_u64() as i64, 0);
+
+        let now: DateTime<Utc> = Utc::now();
+        let time_difference: chrono::Duration = now.signed_duration_since(block_time.unwrap());
+
+        println!(
+            "{}",
+            LogEntry {
+                time: Local::now(),
+                level: LogLevel::Info,
+                message: format!(
+                    "block timestamp: {}, time difference: {:.2?}",
+                    block_timestamp, time_difference
+                ),
+            }
+        );
+    }
+
+    Ok(())
+}
+
+// Amsterdam enode
+// enode://35df251d21ec503b24ace1f5b18bb85d06fa0dc113f069ab399c5b4064d3e6d89a947b2ebe373f0fcceba6a3db1441117a74b535f484ade24113c5e0c5cd4795@5.39.169.121:30303
+
+// USA enode
+// enode://78253a80431ac5514f3efc0aafe7d5793a0e838a97cbc54acbf2fc5219307bc72b96be2747615847e04aabd5495af96ec6f1e661da9e7f71e86717a51a0fd291@54.146.231.114:30303
+
+// maybe lighthouse receives the block before geth does
+
+// try get the beacon node blocks and check how long it takes to receive them from another peer and maybe check how long it takes for geth to receive it from the beacon node
