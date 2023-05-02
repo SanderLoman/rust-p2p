f2a057d1b52c0c0e2403ea9cf064f0305be27a03
SanderLoman <sanderfeitsma13@gmail.com>
2023-05-02 23:56:06 +0200

    updated sandwhich file

--- src/sandwhich.rs
+++ src/sandwhich.rs
@@ -1,24 +1,32 @@
-// #![deny(unsafe_code)]
+#![deny(unsafe_code)]
 
-// use dotenv::dotenv;
-// use ethers::prelude::*;
-// use ethers_flashbots::*;
-// use eyre::Result;
+use dotenv::dotenv;
+use ethers::prelude::*;
+use ethers_flashbots::*;
+use eyre::Result;
 
-// pub async fn sandwhich() -> Result<()> {
-//     dotenv().ok();
-//     println!("Hello from sandwhich!");
-//     Ok(())
-// }
+// probs going to end up merging all of these into one function
 
-// pub async fn sandwhich_arb() -> Result<()> {
-//     dotenv().ok();
-//     println!("Hello from sandwhich_arb!");
-//     Ok(())
-// }
+// normal sandwhich trade
+pub async fn sandwhich() {}
 
-// pub async fn sandwhich_arb_backrun() -> Result<()> {
-//     dotenv().ok();
-//     println!("Hello from sandwhich_arb_backrun!");
-//     Ok(())
-// }
+// sandwhich trade with backrun
+pub async fn sandwhich_backrun() {}
+
+// sandwhich trade with arbitrage
+pub async fn sandwhich_arb() {}
+
+// sandwhich trade with arbitrage and backrun
+pub async fn sandwhich_arb_backrun() {}
+
+// sandwhich trade with multiple tokens
+pub async fn sandwhich_multi() {}
+
+// sandwhich trade with multiple tokens and backrun
+pub async fn sandwhich_multi_backrun() {}
+
+// sandwhich trade with multiple tokens and arbitrage
+pub async fn sandwhich_multi_arb() {}
+
+// sandwhich trade with multiple tokens, arbitrage, and backrun
+pub async fn sandwhich_multi_arb_backrun() {}
