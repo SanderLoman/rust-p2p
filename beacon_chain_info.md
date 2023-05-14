60435fe31a75d3deb3b14e04c9ae83c794e777b2
SanderLoman <sanderfeitsma13@gmail.com>
2023-05-14 23:07:26 +0200

    ignore

---
+++ beacon_chain_info.md
@@ -0,0 +1,28 @@
+# Collection of Beacon Chain Information
+
+## Table of Contents
+
+1. [Discv5](#Discv5)
+2. [libp2p](#libp2p)
+
+## Discv5
+
+- **Discv5**: Discv5 is a protocol used for node discovery. It allows Ethereum nodes to find each other on the network. Once nodes have discovered each other, they can establish connections and start sharing data. However, Discv5 doesn't dictate how these connections should be established or how data should be shared. It's only concerned with node discovery.
+
+### What do we need for Discv5 and discovering peer?
+
+- ENR of the consensus client
+  - 
+- Listenings address
+  - 
+- Discv5 config
+  - 
+- ****
+
+## libp2p
+
+- **libp2p**: libp2p is a modular network stack that can be used to build peer-to-peer applications. It provides a suite of protocols for various aspects of peer-to-peer communication, including transport (how data is physically sent from one node to another), multiplexing (how multiple data streams can be sent over a single connection), peer discovery (how nodes find each other), and more. Ethereum 2.0 uses libp2p for most of its networking needs, including establishing connections between nodes and sharing blockchain data.
+
+### What do we need for libp2p and managing the peers?
+
+- ****
--- src/peers_retry.rs
+++ src/peers_retry.rs
@@ -1,10 +1,16 @@
-use base64;
+/// !!!
+///52062
+/// LISTENING ADDRESS: /ip4/0.0.0.0/tcp/9000/p2p/16Uiu2HAm3CHQXGJokLWodbDocko58tCdgotxYcR6BuXyLKcuobUR
+///
+/// ENR ADDRESS: enr:-K24QGDcHgq97t7pNQ0E4Q-FwiQN3ZT5JDmuMC7hz6A1bIRyO32Sti8NSpclcCTNfPgQvU6L5dgvXRfxLu7L7NeKGUY0h2F0dG5ldHOIAAAAAAAAAACEZXRoMpBiiUHvAwAQIP__________gmlkgnY0iXNlY3AyNTZrMaECc29ruZqHENx-CIWjjqcFRZpVXRmo2h20dbjRHy1fgE6Ic3luY25ldHMAg3RjcIIjKA
+///
+/// !!!
 use chrono::{DateTime, Local, TimeZone, Utc};
 use colored::*;
 use discv5::{
     enr,
     enr::{CombinedKey, NodeId},
-    Discv5, Discv5ConfigBuilder, TokioExecutor,
+    Discv5, Discv5ConfigBuilder, Enr, TokioExecutor,
 };
 use ethers::prelude::*;
 use eyre::Result;
@@ -68,9 +74,147 @@ impl fmt::Display for LogEntry {
     }
 }
 
-pub async fn discover_peers() -> Result<(), Box<dyn Error>> {
-    // get the local peer_id from the beacon node
+// curl -X 'GET' 'http://127.0.0.1:5052/eth/v1/node/peers' -H 'accept: application/json'
+pub async fn bootstrapped_peers() -> Result<Vec<String>, Box<dyn Error>> {
+    let url: &str = "http://127.0.0.1:5052/eth/v1/node/peers";
+    let client: Client = Client::new();
 
-    // setup 
+    let mut headers = HeaderMap::new();
+    headers.insert(ACCEPT, "application/json".parse().unwrap());
+
+    let response: Value = client
+        .get(url)
+        .headers(headers)
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    let data: &Vec<Value> = response.get("data").unwrap().as_array().unwrap();
+
+    let mut connected_peers = Vec::new();
+
+    for peer in data {
+        if let Some(Value::String(address)) = peer.get("last_seen_p2p_address") {
+            if let Some(Value::String(peer_id)) = peer.get("peer_id") {
+                if let Some(Value::String(state)) = peer.get("state") {
+                    if state == "connected" {
+                        connected_peers.push(format!("{}/p2p/{}", address, peer_id));
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(connected_peers)
+}
+
+pub async fn get_local_peer_id() -> Result<String, Box<dyn Error>> {
+    let url = "http://127.0.0.1:5052/eth/v1/node/identity";
+    let client = reqwest::Client::new();
+    let mut headers = HeaderMap::new();
+    headers.insert(ACCEPT, "application/json".parse().unwrap());
+    let res = client.get(url).headers(headers).send().await?;
+    let body = res.text().await?;
+    let json: Value = serde_json::from_str(&body)?;
+    let peer_id = json["data"]["peer_id"]
+        .as_str()
+        .ok_or("Peer ID not found")?
+        .to_owned();
+    Ok(peer_id)
+}
+
+pub async fn get_enr_key() -> Result<String, Box<dyn Error>> {
+    let url = "http://127.0.0.1:5052/eth/v1/node/identity";
+    let client = reqwest::Client::new();
+    let mut headers = HeaderMap::new();
+    headers.insert(ACCEPT, "application/json".parse().unwrap());
+    let res = client.get(url).headers(headers).send().await?;
+    let body = res.text().await?;
+    let json: Value = serde_json::from_str(&body)?;
+    let enr_key = json["data"]["enr"]
+        .as_str()
+        .ok_or("ENR not found")?
+        .to_owned();
+    Ok(enr_key)
+}
+
+// probably need to use the discv5 crate for this since its for discovery
+pub async fn discover_peers() -> Result<(), Box<dyn Error>> {
+    // maybe we dont need the local peer id because we are just discovering peers
+    let local_peer_id = get_local_peer_id().await?;
+    let enr_key = get_enr_key().await?;
+
+    let enr: discv5::enr::Enr<discv5::enr::CombinedKey> = enr::Enr::from_str(&enr_key)?;
+
     Ok(())
 }
+
+// probably need to use the libp2p crate for this since its for managing peers
+pub async fn handle_discovered_peers() -> Result<(), Box<dyn Error>> {
+    let local_peer_id = get_local_peer_id().await?;
+    let enr_key = get_enr_key().await?;
+    Ok(())
+}
+
+/*
+use async_std::task;
+use discv5::{enr::{CombinedKey, Enr, EnrBuilder}, enr_ext::create_enr, enr_key::secp256k1, Discv5Config, Discv5Service};
+use std::error::Error;
+use std::net::SocketAddr;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Generate a random secp256k1 key pair
+    let key = secp256k1::SecretKey::random();
+    let keypair = CombinedKey::from_secret(key);
+
+    // Generate the local ENR (Ethereum Node Record)
+    let local_enr: Enr<CombinedKey> = EnrBuilder::new("v4").build(&keypair)?;
+
+    // Specify the configuration for the Discv5 service
+    let config = Discv5Config {
+        bind_address: SocketAddr::from(([0, 0, 0, 0], 0)), // Replace with desired bind address
+        enr: Some(local_enr.clone()),
+        ..Default::default()
+    };
+
+    // Create a Discv5 service
+    let mut discv5 = Discv5Service::new(keypair, local_enr, config)?;
+
+    // Start the Discv5 service in a separate task
+    let discv5_task = task::spawn(async move {
+        while let Some(event) = discv5.next().await {
+            match event {
+                discv5::Discv5Event::Discovered(enr) => {
+                    println!("Discovered new peer: {:?}", enr);
+                    // Handle the discovered peer (e.g., add it to a peer list)
+                }
+                discv5::Discv5Event::SocketError(err) => {
+                    println!("Discv5 socket error: {:?}", err);
+                    // Handle socket errors
+                }
+                _ => {}
+            }
+        }
+    });
+
+    // Bootstrap the Discv5 service by connecting to known bootstrap nodes
+    let bootstrap_nodes = vec![
+        "enr://enr.example.com?key=value", // Replace with actual bootstrap nodes
+        "enr://enr.example.com?key=value",
+    ];
+    discv5.bootstrap(&bootstrap_nodes)?;
+
+    // Perform any other application-specific tasks
+    // ...
+
+    // Block the main thread to keep the Discv5 service running
+    task::block_on(discv5_task);
+
+    Ok(())
+}
+
+
+
+
+ */
