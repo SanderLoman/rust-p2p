5b9190aa6fc2458f2df55c4f5c6ecdb518256871
SanderLoman <sanderfeitsma13@gmail.com>
2023-07-24 04:48:40 +0200

    continue tmr

--- networking/src/discv5/enr/enr.rs
+++ networking/src/discv5/enr/enr.rs
@@ -1,17 +1,26 @@
 #![deny(unsafe_code)]
 
-use discv5::{enr, Enr, enr::CombinedKey};
+use discv5::{
+    enr::{CombinedKey, EnrBuilder},
+    Enr,
+};
+use reqwest::header::{HeaderMap, HeaderValue, ACCEPT};
+use reqwest::Client;
 use std::error::Error;
 
 async fn get_local_enr() -> Result<Enr, Box<dyn Error>> {
-
-    
-    Ok(enr_key)
+    // start a beacon client for this to work
+    // curl -X 'GET' 'http://127.0.0.1:5052/eth/v1/node/identity' -H 'accept: application/json'
+    Ok()
 }
 
-pub fn generate_enr() -> Result<Enr, Box<dyn Error>> {
-    let enr_combined_key = enr::CombinedKey::generate_secp256k1();
-    let enr_key = enr::EnrBuilder::new("v4").build(&enr_combined_key).unwrap();
+pub async fn generate_enr() -> Result<Enr, Box<dyn Error>> {
+    let enr_combined_key: CombinedKey = CombinedKey::generate_secp256k1();
+    let enr_key: discv5::enr::Enr<CombinedKey> = EnrBuilder::new("v4")
+        .add_value("", "")
+        .add_value("", "")
+        .add_value("", "")
+        .build(&enr_combined_key)?;
 
-    Ok(enr_key)
+    Ok(())
 }
