4b655377730d0df1653f278fe4723bb4a9103fbe
SanderLoman <sanderfeitsma13@gmail.com>
2023-08-01 05:20:59 +0200

    continue on swarm tmr

--- networking/src/libp2p/swarm/swarm.rs
+++ networking/src/libp2p/swarm/swarm.rs
@@ -1,16 +1,14 @@
 #![deny(unsafe_code)]
 
 use libp2p::{
-    swarm::SwarmBuilder,
-    PeerId,
-    Multiaddr,
-    Swarm,
-    mdns::{Mdns, MdnsConfig},
-    noise::{NoiseConfig, X25519Spec, Keypair},
-    yamux::YamuxConfig,
     identity,
+    mdns::{Mdns, MdnsConfig},
     mplex::MplexConfig,
+    noise::{Keypair, NoiseConfig, X25519Spec},
+    swarm::SwarmBuilder,
     swarm::SwarmEvent,
+    yamux::YamuxConfig,
+    Multiaddr, PeerId, Swarm,
 };
 
 // #[derive(NetworkBehaviour)]
@@ -18,35 +16,51 @@ struct Behaviour {
     // Define your network behaviour here
 }
 
-fn main() {
-    let local_key = identity::Keypair::generate_ed25519();
-    let local_peer_id = PeerId::from(local_key.public());
+pub async fn run() {
+    // Create a random PeerId
+    let id_keys = identity::Keypair::generate_ed25519();
+    let peer_id = PeerId::from(id_keys.public());
 
-    let transport = libp2p::development_transport(local_key.clone()).unwrap();
+    // Create a transport.
+    let transport = libp2p::development_transport(id_keys).await.unwrap();
 
-    let behaviour = Behaviour {
-        // Initialize your network behaviour here
+    // Create a Swarm to manage peers and events.
+    let mut swarm = {
+        // Create a Kademlia behaviour.
+        let behaviour = Behaviour {};
+
+        // Create a Swarm to manage peers and events.
+        let executor = {
+            let executor = tokio::runtime::Handle::current();
+            move |_| executor.enter(|| ())
+        }
+        SwarmBuilder::with_executor(transport, behaviour, peer_id, executor)
     };
 
-    let swarm = SwarmBuilder::new(transport, behaviour, local_peer_id)
-        .executor(Box::new(|fut| {
-            async_std::task::spawn(fut);
-        }))
-        .build();
+    // Listen on all interfaces and whatever port the OS assigns.
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse().unwrap()).unwrap();
 
-    async_std::task::block_on(async {
-        let mut swarm = swarm;
+    // Reach out to another node.
+    if let Some(to_dial) = std::env::args().nth(1) {
+        let addr = to_dial.parse::<Multiaddr>().unwrap();
+        swarm.dial_addr(addr).unwrap();
+        println!("Dialed {:?}", to_dial);
+    }
 
-        // Listen on all interfaces and whatever port the OS assigns
-        swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse().unwrap()).unwrap();
-
-        loop {
-            match swarm.select_next_some().await {
-                SwarmEvent::Behaviour(event) => {
-                    // Handle behaviour events here
+    // Kick it off.
+    let mut listening = false;
+    let mut listening_addr = None;
+    loop {
+        match swarm.next_event().await {
+            SwarmEvent::NewListenAddr { address, .. } => {
+                if !listening {
+                    listening = true;
+                    listening_addr = Some(address);
+                    println!("Listening on {:?}", address);
                 }
-                _ => {}
             }
+            SwarmEvent::Behaviour(_) => {}
+            _ => {}
         }
-    });
+    }
 }
