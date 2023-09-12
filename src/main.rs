a8fe9368e91abcd6599f50b2f4aa606e6e7ad296
SanderLoman <sanderfeitsma13@gmail.com>
2023-09-12 21:22:14 +0200

    logging examples

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use dotenv::dotenv;
 use eyre::Result;
 use std::error::Error;
 
+use slog::{warn, error, crit, debug, trace, info};
+
 use wagmi::{create_logger, parse_verbosity};
 
 #[tokio::main()]
@@ -11,6 +13,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
     let verbosity = parse_verbosity();
     let log = create_logger(verbosity);
+
+
+    info!(log, "Starting wagmi");
+    warn!(log, "This is a warning");
+    error!(log, "This is an error");
+    crit!(log, "This is a critical error");
+    debug!(log, "This is a debug message");
+    trace!(log, "This is a trace message");
 
     Ok(())
 }
