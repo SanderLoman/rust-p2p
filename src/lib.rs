a99b2ba8ad13f2bae0731fccbfc3e099791d1c5d
SanderLoman <sanderfeitsma13@gmail.com>
2023-09-12 21:22:05 +0200

    improved readability in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -46,10 +46,24 @@ pub fn parse_verbosity() -> Vec<Level> {
 
     // Map the verbosity level to a vector of slog::Level
     match verbosity {
-        1 => vec![Level::Info],
-        2 => vec![Level::Info, Level::Warning],
-        3 => vec![Level::Info, Level::Warning, Level::Error],
-        4 => vec![Level::Info, Level::Warning, Level::Error, Level::Critical],
+        1 => vec![
+            Level::Info
+            ],
+        2 => vec![
+            Level::Info,
+            Level::Warning
+            ],
+        3 => vec![
+            Level::Info,
+            Level::Warning,
+            Level::Error
+            ],
+        4 => vec![
+            Level::Info,
+            Level::Warning,
+            Level::Error,
+            Level::Critical
+            ],
         5 => vec![
             Level::Info,
             Level::Warning,
