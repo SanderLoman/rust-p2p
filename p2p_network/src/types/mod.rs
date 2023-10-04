e92157c6e9f28fe3f214216c3f57827ceac06241
SanderLoman <sanderfeitsma13@gmail.com>
2023-10-04 10:42:09 +0200

    added eth_spec and bitvector files

--- p2p_network/src/types/mod.rs
+++ p2p_network/src/types/mod.rs
@@ -1,23 +1,22 @@
-// pub mod error;
-// pub mod globals;
-// pub mod pubsub;
-// pub mod subnet;
-// pub mod sync_state;
-// pub mod topics;
+pub mod error;
+mod globals;
+mod pubsub;
+mod subnet;
+mod sync_state;
+mod topics;
 
-// pub type EnrAttestationBitfield<T> = BitVector<<T as EthSpec>::SubnetBitfieldLength>;
-// pub type EnrSyncCommitteeBitfield<T> = BitVector<<T as EthSpec>::SyncCommitteeSubnetCount>;
+use project_types::{BitVector, EthSpec};
 
-// pub type Enr = discv5::enr::Enr<discv5::enr::CombinedKey>;
+pub type EnrAttestationBitfield<T> = BitVector<<T as EthSpec>::SubnetBitfieldLength>;
+pub type EnrSyncCommitteeBitfield<T> = BitVector<<T as EthSpec>::SyncCommitteeSubnetCount>;
 
-// pub use globals::NetworkGlobals;
-// pub use pubsub::{PubsubMessage, SnappyTransform};
-// pub use subnet::{Subnet, SubnetDiscovery};
-// pub use sync_state::{BackFillState, SyncState};
-// pub use topics::{
-//     core_topics_to_subscribe, fork_core_topics, subnet_from_topic_hash, GossipEncoding, GossipKind,
-//     GossipTopic, LIGHT_CLIENT_GOSSIP_TOPICS,
-// };
+pub type Enr = discv5::enr::Enr<discv5::enr::CombinedKey>;
 
-// //    error, Enr, EnrSyncCommitteeBitfield, GossipTopic, NetworkGlobals, PubsubMessage, Subnet,
-// //    SubnetDiscovery,
+pub use globals::NetworkGlobals;
+pub use pubsub::{PubsubMessage, SnappyTransform};
+pub use subnet::{Subnet, SubnetDiscovery};
+pub use sync_state::{BackFillState, SyncState};
+pub use topics::{
+    core_topics_to_subscribe, fork_core_topics, subnet_from_topic_hash, GossipEncoding, GossipKind,
+    GossipTopic, LIGHT_CLIENT_GOSSIP_TOPICS,
+};
