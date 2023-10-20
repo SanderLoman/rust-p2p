4cd3ef7ee316aefceb55105bfd559e5378a8be45
SanderLoman <sanderfeitsma13@gmail.com>
2023-10-20 23:33:50 +0200

    discovery

--- proxy/src/network/discovery/mod.rs
+++ proxy/src/network/discovery/mod.rs
@@ -8,10 +8,14 @@ use discv5::{
     enr as discv5_enr, enr::CombinedKey, handler, kbucket, metrics, packet, permit_ban, rpc,
     service, socket, Discv5, Discv5Config, Discv5ConfigBuilder, Discv5Event, Enr, ListenConfig,
 };
+use futures::stream::FuturesUnordered;
+use futures::Future;
 use libp2p::PeerId;
-use slog::Logger;
+use slog::{debug, error, Logger};
+use std::pin::Pin;
 use std::time::Duration;
 use std::{collections::HashMap, time::Instant};
+use tokio::sync::mpsc;
 // use futures::Future;
 // use libp2p::swarm::dummy::ConnectionHandler;
 // use libp2p::swarm::NetworkBehaviour;
@@ -23,14 +27,31 @@ use self::enr::generate_enr;
 // use std::pin::Pin;
 // use void::Void;
 
+enum EventStream {
+    /// Awaiting an event stream to be generated. This is required due to the poll nature of
+    /// `Discovery`
+    Awaiting(
+        Pin<
+            Box<
+                dyn Future<Output = Result<mpsc::Receiver<Discv5Event>, discv5::Discv5Error>>
+                    + Send,
+            >,
+        >,
+    ),
+    /// The future has completed.
+    Present(mpsc::Receiver<Discv5Event>),
+    // The future has failed or discv5 has been disabled. There are no events from discv5.
+    InActive,
+}
+
 #[derive(Debug)]
 pub struct DiscoveredPeers {
     pub peers: HashMap<PeerId, Option<Instant>>,
 }
 
 pub struct Discovery {
-    enr: Enr,
     discv5: Discv5,
+    event_stream: EventStream,
     log: Logger,
 }
 
@@ -54,17 +75,70 @@ impl Discovery {
             .ping_interval(Duration::from_secs(300))
             .build();
 
-        let discv5: Discv5 = Discv5::new(enr.clone(), enr_key, discv5_config)?;
+        let mut discv5: Discv5 = Discv5::new(enr.clone(), enr_key, discv5_config)?;
 
-        Ok(Discovery {
-            enr,
+        let event_stream = {
+            discv5.start().await.unwrap();
+            debug!(log, "Discv5 started"; "enr" => ?enr);
+            EventStream::Awaiting(Box::pin(discv5.event_stream()))
+        };
+
+        // !!! TODO: !!!
+        //
+        // Instead of using the `boot_nodes_multiaddr` from the config, we should use have another file with all the found ENRs, and use that instead.
+        //
+        // // get futures for requesting the Enrs associated to these multiaddr and wait for their
+        // // completion
+        // let mut fut_coll = config
+        //     .boot_nodes_multiaddr
+        //     .iter()
+        //     .map(|addr| addr.to_string())
+        //     // request the ENR for this multiaddr and keep the original for logging
+        //     .map(|addr| {
+        //         futures::future::join(
+        //             discv5.request_enr(addr.clone()),
+        //             futures::future::ready(addr),
+        //         )
+        //     })
+        //     .collect::<FuturesUnordered<_>>();
+
+        // while let Some((result, original_addr)) = fut_coll.next().await {
+        //     match result {
+        //         Ok(enr) => {
+        //             debug!(
+        //                 log,
+        //                 "Adding node to routing table";
+        //                 "node_id" => %enr.node_id(),
+        //                 "peer_id" => %enr.peer_id(),
+        //                 "ip" => ?enr.ip4(),
+        //                 "udp" => ?enr.udp4(),
+        //                 "tcp" => ?enr.tcp4(),
+        //                 "quic" => ?enr.quic4()
+        //             );
+        //             let _ = discv5.add_enr(enr).map_err(|e| {
+        //                 error!(
+        //                     log,
+        //                     "Could not add peer to the local routing table";
+        //                     "addr" => original_addr.to_string(),
+        //                     "error" => e.to_string(),
+        //                 )
+        //             });
+        //         }
+        //         Err(e) => {
+        //             error!(log, "Error getting mapping to ENR"; "multiaddr" => original_addr.to_string(), "error" => e.to_string())
+        //         }
+        //     }
+        // }
+
+        Ok(Self {
             discv5,
+            event_stream,
             log,
         })
     }
 
     pub fn get_enr(&self) -> Enr {
-        self.enr.clone()
+        self.discv5.local_enr().clone()
     }
 
     pub async fn start(&mut self) -> Result<(), Box<dyn Error>> {
