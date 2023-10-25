1b32c58346aa4ddeb7e767aa07a10eb0b2328e03
SanderLoman <sanderfeitsma13@gmail.com>
2023-10-25 03:41:27 +0200

    small tests

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,18 @@ use std::error::Error;
 
 use wagmi::{create_logger, parse_verbosity};
 
+use proxy::get_lh_tcp_multiaddr;
+use proxy::network::discovery::Discovery;
+
 #[tokio::main()]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
     let verbosity = parse_verbosity();
     let log = create_logger(verbosity);
+
+    get_lh_tcp_multiaddr(log.clone()).await?;
+
+    Discovery::new(log.clone()).await?;
 
     Ok(())
 }
