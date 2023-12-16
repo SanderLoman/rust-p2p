843b087f8c9fd224ca519b6230bce4e79695e8e2
SanderLoman <sanderfeitsma13@gmail.com>
2023-12-16 05:25:20 +0100

    Update dependencies and remove unused code

--- contower/src/main.rs
+++ contower/src/main.rs
@@ -4,10 +4,15 @@ use dotenv::dotenv;
 use eyre::Result;
 use std::error::Error;
 
-use wagmi::{create_logger, parse_verbosity};
+use contower::{create_logger, parse_verbosity};
 
 #[tokio::main()]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // Enable backtraces unless a RUST_BACKTRACE value has already been explicitly provided.
+    if std::env::var("RUST_BACKTRACE").is_err() {
+        std::env::set_var("RUST_BACKTRACE", "1");
+    }
+
     dotenv().ok();
     let verbosity = parse_verbosity();
     let _log = create_logger(verbosity);
